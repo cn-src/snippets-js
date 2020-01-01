ee1a4d49ab47c172e3aa7b2afbc1b789ef840b65
cn-src <public@javaer.cn>
2020-01-01 08:05:06 +0800

    update

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  coverageDirectory: "./coverage/",
+  collectCoverage: true,
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
