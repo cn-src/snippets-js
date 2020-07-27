19e9ca8a7b4a96a688219cf174b7dfb23f6d002c
cn-src <public@javaer.cn>
2020-07-27 23:53:15 +0800

    update

--- src/AxiosClient.ts
+++ src/AxiosClient.ts
@@ -2,19 +2,26 @@ import { AxiosStatic } from "axios";
 import stringify from "./qs/stringify";
 
 /**
+ * 属性全部为简单类型的对象
+ */
+type ST = { [propName: string]: string | number | boolean }
+
+/**
  * 根据 axios 创建一个新的 AxiosClient
  */
 function AxiosClient(axios: AxiosStatic) {
+
+  /* eslint-disable @typescript-eslint/ban-types */
   return {
     /**
      * GET 请求
      */
-    get(url: string) {
-      return async function (params?: any, pathVariables?: any) {
+    get<P extends ST, V extends ST>(url: string) {
+      return async function(params?: P, pathVariables?: V) {
         const promise = await axios.request({
           url: pathParse(url, pathVariables),
           method: "GET",
-          params,
+          params
         });
         return promise.data;
       };
@@ -22,12 +29,12 @@ function AxiosClient(axios: AxiosStatic) {
     /**
      * POST 请求
      */
-    post(url: string) {
-      return async function (data?: any, pathVariables?: any) {
+    post<D extends object, V extends ST>(url: string) {
+      return async function(data?: D, pathVariables?: V) {
         const promise = await axios.request({
           url: pathParse(url, pathVariables),
           method: "POST",
-          data,
+          data
         });
         return promise.data;
       };
@@ -35,12 +42,12 @@ function AxiosClient(axios: AxiosStatic) {
     /**
      * POST 请求，Content-Type 为 application/x-www-form-urlencoded
      */
-    postForm(url: string) {
-      return async function (data: any, pathVariables?: any) {
+    postForm<D extends ST, V extends ST>(url: string) {
+      return async function(data: D, pathVariables?: V) {
         const promise = await axios.request({
           url: pathParse(url, pathVariables),
           method: "POST",
-          data: stringify(data),
+          data: stringify(data)
         });
         return promise.data;
       };
@@ -48,36 +55,36 @@ function AxiosClient(axios: AxiosStatic) {
     /**
      * POST 请求，Content-Type 为 multipart/form-data
      */
-    postFormData(url: string) {
-      return async function (data: any, pathVariables?: any) {
+    postFormData<D extends { [propName: string]: string | Blob }, V extends ST>(url: string) {
+      return async function(data: D, pathVariables?: V) {
         // eslint-disable-next-line no-undef
         const formData = new FormData();
         for (const key in data) {
           if (Object.prototype.hasOwnProperty.call(data, key)) {
             formData.append(key, data[key]);
           }
-          data = formData;
         }
         const promise = await axios.request({
           url: pathParse(url, pathVariables),
           method: "POST",
-          data: formData,
+          data: formData
         });
         return promise.data;
       };
-    },
+    }
   };
+  /* eslint-enable @typescript-eslint/ban-types */
 }
 
 /**
  * 路径变量解析
  */
-function pathParse(path: string, pathVariables?: any) {
+function pathParse(path: string, pathVariables?: ST) {
   let rs = path;
   if (pathVariables) {
     for (const key in pathVariables) {
       if (Object.prototype.hasOwnProperty.call(pathVariables, key)) {
-        rs = path.replace(`{${key}}`, pathVariables[key]);
+        rs = path.replace(`{${key}}`, "" + pathVariables[key]);
       }
     }
   }
