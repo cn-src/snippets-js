1d182a6334c163a8b84900ceb0bb05dd3e89d800
cn-src <public@javaer.cn>
2020-07-28 11:28:10 +0800

    update

--- src/AxiosClient.ts
+++ src/AxiosClient.ts
@@ -4,28 +4,29 @@ import stringify from "./qs/stringify";
 /**
  * 属性全部为简单类型的对象
  */
-type SIMPLE_OBJECT = { [propName: string]: string | number | boolean }
+type SIMPLE_OBJECT = { [propName: string]: string | number | boolean };
 
 /**
  * 类似 JSON 一样，属性以及子属性全部为简单类型
  */
-type JSON_OBJECT = { [propName: string]: string | number | boolean | JSON_OBJECT }
+type JSON_OBJECT = {
+  [propName: string]: string | number | boolean | JSON_OBJECT;
+};
 
 /**
  * 根据 axios 创建一个新的 AxiosClient
  */
 function AxiosClient(axios: AxiosStatic) {
-
   return {
     /**
      * GET 请求
      */
-    get<P extends SIMPLE_OBJECT, V extends SIMPLE_OBJECT>(url: string) {
-      return async function(params?: P, pathVariables?: V) {
+    get<P = SIMPLE_OBJECT, V = SIMPLE_OBJECT>(url: string) {
+      return async function (params?: P, pathVariables?: V) {
         const promise = await axios.request({
           url: pathParse(url, pathVariables),
           method: "GET",
-          params
+          params,
         });
         return promise.data;
       };
@@ -33,12 +34,12 @@ function AxiosClient(axios: AxiosStatic) {
     /**
      * POST 请求
      */
-    post<D extends JSON_OBJECT, V extends SIMPLE_OBJECT>(url: string) {
-      return async function(data?: D, pathVariables?: V) {
+    post<D = JSON_OBJECT, V = SIMPLE_OBJECT>(url: string) {
+      return async function (data?: D, pathVariables?: V) {
         const promise = await axios.request({
           url: pathParse(url, pathVariables),
           method: "POST",
-          data
+          data,
         });
         return promise.data;
       };
@@ -46,12 +47,12 @@ function AxiosClient(axios: AxiosStatic) {
     /**
      * POST 请求，Content-Type 为 application/x-www-form-urlencoded
      */
-    postForm<D extends SIMPLE_OBJECT, V extends SIMPLE_OBJECT>(url: string) {
-      return async function(data: D, pathVariables?: V) {
+    postForm<D = SIMPLE_OBJECT, V = SIMPLE_OBJECT>(url: string) {
+      return async function (data: D, pathVariables?: V) {
         const promise = await axios.request({
           url: pathParse(url, pathVariables),
           method: "POST",
-          data: stringify(data)
+          data: stringify(data),
         });
         return promise.data;
       };
@@ -59,31 +60,32 @@ function AxiosClient(axios: AxiosStatic) {
     /**
      * POST 请求，Content-Type 为 multipart/form-data
      */
-    postFormData<D extends { [propName: string]: string | Blob }, V extends SIMPLE_OBJECT>(url: string) {
-      return async function(data: D, pathVariables?: V) {
+    postFormData<D = { [propName: string]: string | Blob }, V = SIMPLE_OBJECT>(
+      url: string
+    ) {
+      return async function (data: D, pathVariables?: V) {
         // eslint-disable-next-line no-undef
         const formData = new FormData();
         for (const key in data) {
           if (Object.prototype.hasOwnProperty.call(data, key)) {
-            formData.append(key, data[key]);
+            formData.append(key, data[key] as any);
           }
         }
         const promise = await axios.request({
           url: pathParse(url, pathVariables),
           method: "POST",
-          data: formData
+          data: formData,
         });
         return promise.data;
       };
-    }
+    },
   };
-
 }
 
 /**
  * 路径变量解析
  */
-function pathParse(path: string, pathVariables?: SIMPLE_OBJECT) {
+function pathParse<T = SIMPLE_OBJECT>(path: string, pathVariables?: T) {
   let rs = path;
   if (pathVariables) {
     for (const key in pathVariables) {
